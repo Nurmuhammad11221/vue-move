1276b0f870d4a1944109a29c9ac662aba6853cb6
Nurmuhamamd11221 <roziyaabdurazoqova@gmail.com>
2024-10-25 15:32:37 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <Nav />
+</template>
+
+<script>
+import Nav from './components/Nav.vue';
+
+
+export default {
+    components: {
+        Nav,
+        
+    }
+}
+</script>
+
+<style>
+* {
+    padding: 0;
+    margin: 0%;
+    text-decoration: none;
+}
+
+</style>
